eca899cd45f39c723410a87fd35867a4cfae3eea
Xuewei Ouyang <xouyang@princeton.edu>
2016-04-06 14:21:24 -0400

    fixed model bugs

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -6,10 +6,9 @@ from django.contrib.auth.models import User
 
 class Pet(models.Model):
     name = models.CharField(max_length=255)
-    created = models.DateTimeField('auto_now_add=True')
+    created = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField()
-    def __str__(self):
-    	return self.pet_text
+
 
 class Owner(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -21,12 +20,14 @@ class Owner(models.Model):
 	#saveNum =
 	baseCost = models.IntegerField()
 	numTrans = models.PositiveIntegerField()
-	lastPay = models.DateField() # need to retrieve Payment
+	lastPay = models.DateField() 
+	# need to retrieve Payment
+	# what to do when user first initialized and no last payment?
 
 
 class Payment(models.Model):
 	owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
-	date = models.DateField('auto_now_add=True')
+	date = models.DateField(auto_now_add=True)
 	amount = models.PositiveIntegerField()
 
 
