ed145567b9a3f439a197101da6fd9cbe84a43b82
Peter Chen <pcchen@princeton.edu>
2016-05-09 13:45:04 -0400

    Created scheduled task for genauthtoken.py.

--- genauthtoken.py
+++ genauthtoken.py
@@ -1,35 +1,27 @@
 import requests
 import json
 import sys
-
-#refresh token YkGO975Bcttuxq5xm4tuhYelJwBfIyhjquGkDIsqcsq20f3yxP
-
-url = 'https://uat.dwolla.com/oauth/v2/token'
-
-payload = {
-  "client_id": "KORB1uPLiCs2vO96B4Hiwxf8PsQ3Vk43I4k4oopegs5HjrmkLd",
-  "client_secret": "6919gTg6EcX1EK2YulRvDgWeDuMTvIFUt3krwux52e4REeP7Mq",
-  "refresh_token": sys.argv[1],
-  "grant_type": "refresh_token"
-}
-
-new_tok = requests.post(url, data=payload)
-parsed_json = json.loads(new_tok.text)
-# print parsed_json['refresh_token']
-print parsed_json['access_token']
+import time
 
 
+def gentoken(inputtoken):
+	url = 'https://uat.dwolla.com/oauth/v2/token'
 
-# client_id = 'KORB1uPLiCs2vO96B4Hiwxf8PsQ3Vk43I4k4oopegs5HjrmkLd'
-# redirect_uri = 'https://tokengenerator.dwolla.com/redirect'
-# scope = 'send|transactions|funding|managecustomers'
+	payload = {
+	  "client_id": "KORB1uPLiCs2vO96B4Hiwxf8PsQ3Vk43I4k4oopegs5HjrmkLd",
+	  "client_secret": "6919gTg6EcX1EK2YulRvDgWeDuMTvIFUt3krwux52e4REeP7Mq",
+	  "refresh_token": inputtoken,
+	  "grant_type": "refresh_token"
+	}
 
-# url = 'https://uat.dwolla.com/oauth/v2/authenticate?client_id=' + client_id + '&response_type=code&redirect_uri=' + redirect_uri + '&scope=' + scope
+	new_tok = requests.post(url, data=payload)
+	parsed_json = json.loads(new_tok.text)
+	refreshtoken = parsed_json['refresh_token']
+	accesstoken = parsed_json['access_token']
+	print refreshtoken
+	print accesstoken
 
-# # payload = {
-# #   "client_id": "KORB1uPLiCs2vO96B4Hiwxf8PsQ3Vk43I4k4oopegs5HjrmkLd",
-# #   "client_secret": "6919gTg6EcX1EK2YulRvDgWeDuMTvIFUt3krwux52e4REeP7Mq",
-# #   "grant_type": "client_credentials"
-# # }
+	time.sleep(3000)
+	gentoken(refreshtoken)
 
-# new_tok = requests.post(url)
+gentoken(sys.argv[1])
