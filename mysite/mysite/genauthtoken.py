1742f41880b416b2c2dfeaa2bbdd1a0c324b9c07
Peter Chen <pcchen@princeton.edu>
2016-05-09 20:08:39 -0400

    Cleaned up bad changes to genauthtoken.py.

--- mysite/mysite/genauthtoken.py
+++ mysite/mysite/genauthtoken.py
@@ -4,36 +4,25 @@ import sys
 import time
 
 
-<<<<<<< HEAD:genauthtoken.py
-def gentoken():
-	frefresh = open('refresh_token.txt', 'r')
-	inputtoken = frefresh.read()
-	print inputtoken
-=======
 def genToken():
->>>>>>> 299c7dd7ca1fc4f344bd1a8628420062b6991698:mysite/mysite/genauthtoken.py
+	frefresh = open('refresh_token.txt', 'w+')
+	inputtoken = frefresh.read()
 	url = 'https://uat.dwolla.com/oauth/v2/token'
 
 	payload = {
 	  "client_id": "KORB1uPLiCs2vO96B4Hiwxf8PsQ3Vk43I4k4oopegs5HjrmkLd",
 	  "client_secret": "6919gTg6EcX1EK2YulRvDgWeDuMTvIFUt3krwux52e4REeP7Mq",
-	  "refresh_token": "4jg4SaM6QdXJbN3TUyCGPqgnTnozCHwx7NLfjPux4dDXlSSUhi",
+	  "refresh_token": inputtoken,
 	  "grant_type": "refresh_token"
 	}
 
 	new_tok = requests.post(url, data=payload)
 	parsed_json = json.loads(new_tok.text)
-<<<<<<< HEAD:genauthtoken.py
 	newRefresh = parsed_json['refresh_token']
 	newAccess = parsed_json['access_token']
 
-	frefresh = open('refresh_token.txt', 'w+')
 	faccess = open('access_token.txt', 'w+')
 	faccess.write(newAccess)
 	frefresh.write(newRefresh)
 
 gentoken()
-=======
-	return parsed_json['refresh_token']
-gentoken(sys.argv[1])
->>>>>>> 299c7dd7ca1fc4f344bd1a8628420062b6991698:mysite/mysite/genauthtoken.py
